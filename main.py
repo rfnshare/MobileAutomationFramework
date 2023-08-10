77bd17d449786bdd28abbc9e81395a5670b6868a
Abdullah- Al- Faroque <rfnshare@gmail.com>
2023-08-10 13:36:53 +0600

    added unnitest

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
+import unittest
 
 
-class BaseTest:
-    def __init__(self):
-        self.driver = None
-
-    def ConfigureAppium(self):
-        # service = AppiumService()
+class BaseTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
         options = UiAutomator2Options()
-        # service.start()
         options.avd = "Pixel_3a_API_34_extension_level_7_x86_64"
         options.app = "D:/resources/ApiDemos-debug.apk"
-        self.driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+        cls.driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
 
-    def tearDown(self):
-        self.driver.close()
+    @classmethod
+    def tearDownClass(cls):
+        if cls.driver is not None:
+            cls.driver.quit()
--- test.py
+++ test.py
@@ -1,23 +1,14 @@
 from time import sleep
-
-from appium import webdriver
-from appium.options.android import UiAutomator2Options
-from appium.webdriver.appium_service import AppiumService
-import subprocess
-
 from appium.webdriver.common.appiumby import AppiumBy
-from selenium.webdriver.common.by import By
-
+import unittest
 from main import BaseTest
 
 
 class AppiumBasics(BaseTest):
-    def WiFiSettingsName(self):
-        self.ConfigureAppium()
+    def test_WiFiSettingsName(self):
         self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='Preference').click()
         sleep(5)
-        self.tearDown()
 
 
-obj = AppiumBasics()
-obj.WiFiSettingsName()
+if __name__ == '__main__':
+    unittest.main()
