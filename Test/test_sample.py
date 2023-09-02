eafbc7bb3992a6ef1297d7c8bed83484fd7a2abd
Abdullah- Al- Faroque <rfnshare@gmail.com>
2023-09-02 13:50:07 +0600

    Validate Price sum with full functional testing

--- Test/test_sample.py
+++ Test/test_sample.py
@@ -6,12 +6,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 @pytest.mark.usefixtures("setup")
 class TestHomePage:
     def scroll_to_text(self, txt):
         scroll_expression = f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(' \
                             f'new UiSelector().text("{txt}"))'
         self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, scroll_expression)
+
+    def clear_amount(self, price_str):
+        # Remove the "$" symbol and any commas from the string
+        cleaned_price_str = price_str.replace("$", "").replace(",", "")
+
+        try:
+            # Convert the cleaned string to a float
+            price_float = float(cleaned_price_str)
+            return price_float
+        except ValueError:
+            # Handle any conversion errors
+            return None
 
     def test_fill_form(self):
         driver = self.driver
@@ -49,7 +62,8 @@ class TestHomePage:
         for i in range(0, len(products)):
             print("->", products[i].text)
             if products[i].text == "Jordan 6 Rings":
-                (self.driver.find_elements(by=By.ID, value="com.androidsample.generalstore:id/productAddCart"))[i].click()
+                (self.driver.find_elements(by=By.ID, value="com.androidsample.generalstore:id/productAddCart"))[
+                    i].click()
         self.driver.find_element(by=By.ID, value='com.androidsample.generalstore:id/appbar_btn_cart').click()
 
         # validating product name
@@ -65,4 +79,32 @@ class TestHomePage:
         p_p = self.driver.find_element(by=By.ID, value="com.androidsample.generalstore:id/totalAmountLbl").text
         print(p_p)
         assert "165.0" in p_p
+
+    def test_price_validate(self):
+        self.test_fill_form()
+        self.scroll_to_text("Jordan 6 Rings")
+        self.driver.find_element(by=By.XPATH, value="//android.widget.TextView[@text='Jordan 6 "
+                                                    "Rings']/parent::android.widget.LinearLayout//android.widget.TextView[@text='ADD TO CART']").click()
+        self.scroll_to_text("PG 3")
+        self.driver.find_element(by=By.XPATH, value="//android.widget.TextView[@text='PG "
+                                                    "3']/parent::android.widget.LinearLayout//android.widget.TextView[@text='ADD TO CART']").click()
+        self.driver.find_element(by=By.ID, value='com.androidsample.generalstore:id/appbar_btn_cart').click()
+
+        # Waiting for cart page
+        cart = (By.XPATH, "//android.widget.TextView[@text='Cart']")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.text_to_be_present_in_element(cart, "Cart"))
+
+        prices = self.driver.find_elements(by=By.ID, value='com.androidsample.generalstore:id/productPrice')
+        count = 0
+        for price in prices:
+            clean_price = self.clear_amount(price.text)
+            count += clean_price
+        print("Count Price", count)
+        # validating product price
+        total_price = self.driver.find_element(by=By.ID, value="com.androidsample.generalstore:id/totalAmountLbl").text
+        print(total_price)
+        assert count == self.clear_amount(total_price)
+
+        time.sleep(5)
 
