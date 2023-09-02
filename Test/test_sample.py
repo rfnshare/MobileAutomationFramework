a17f232981f42ec928f79f7f3151c671a44885ba
Abdullah- Al- Faroque <rfnshare@gmail.com>
2023-09-02 11:36:33 +0600

    Basic fillup form automation

--- Test/test_sample.py
+++ Test/test_sample.py
@@ -1,3 +1,4 @@
+import time
 from time import sleep
 import pytest
 from appium.webdriver.common.appiumby import AppiumBy
@@ -6,6 +7,18 @@ from selenium.webdriver.common.by import By
 
 @pytest.mark.usefixtures("setup")
 class TestHomePage:
-    def test_sample(self):
-        pass
-        sleep(10)
+    def scroll_to_text(self, txt):
+        scroll_expression = f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(' \
+                            f'new UiSelector().text("{txt}"))'
+        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, scroll_expression)
+    def test_fill_form(self):
+        driver = self.driver
+        driver.implicitly_wait(10)
+        driver.find_element(by=By.ID, value='com.androidsample.generalstore:id/spinnerCountry').click()
+        self.scroll_to_text("Bangladesh")
+        driver.find_element(by=By.XPATH, value="//android.widget.TextView[@text='Bangladesh']").click()
+        driver.find_element(by=By.ID, value="com.androidsample.generalstore:id/nameField").send_keys("Faroque")
+        driver.hide_keyboard()
+        driver.find_element(by=By.XPATH, value="//android.widget.RadioButton[@resource-id='com.androidsample.generalstore:id/radioFemale']").click()
+        driver.find_element(by=By.ID, value='com.androidsample.generalstore:id/btnLetsShop').click()
+        time.sleep(5)
