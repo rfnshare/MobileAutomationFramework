f6c97ff740c9c0b5286b2b965b8ace2a077eb076
Abdullah Al Faroque <rfnshare@gmail.com>
2023-09-08 19:25:02 +0600

    testing with different data

--- utils/locators/android_locators.py
+++ utils/locators/android_locators.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from utils.data import TestData
 
+
 class CommonLocator(object):
     pass
 
@@ -9,7 +10,11 @@ class LoginPageLocator(object):
     COUNTRY_DROPDOWN = (By.ID, "com.androidsample.generalstore:id/spinnerCountry")
     COUNTRY = (By.XPATH, f"//android.widget.TextView[@text='{TestData.COUNTRY}']")
     NAME_FIELD = (By.ID, "com.androidsample.generalstore:id/nameField")
-    GENDER = (By.XPATH, f"//android.widget.RadioButton[@resource-id='com.androidsample.generalstore:id/radio{TestData.GENDER}']")
+    GENDER = (
+        By.XPATH,
+        f"//android.widget.RadioButton[@resource-id='com.androidsample.generalstore:id/radio{TestData.GENDER}']")
     LETS_SHOP = (By.ID, "com.androidsample.generalstore:id/btnLetsShop")
+
+
 class HomePageLocator(object):
     pass
