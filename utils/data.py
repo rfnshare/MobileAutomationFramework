05f54d8bd84915918adcbc8c591598ab7373e45f
Abdullah Al Faroque <rfnshare@gmail.com>
2023-09-08 14:33:44 +0600

    added test sample data

--- utils/data.py
+++ utils/data.py
@@ -7,3 +7,6 @@ class TestData:
     ERR_MSG = "Please enter your name"
     PRODUCT_ONE = "Jordan 6 Rings"
     PRODUCT_ONE_PRICE = "165.0"
+    PRODUCT_TWO = "PG 3"
+    PRODUCT_TWO_PRICE = ""
+    TOC_TITLE = "Terms Of Conditions"
