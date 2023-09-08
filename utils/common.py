e03b4eaa0b999de19129deb887bdeaf5bcdcbe26
Abdullah Al Faroque <rfnshare@gmail.com>
2023-09-08 17:55:58 +0600

    Advanced Logging Config with .conf file

--- utils/common.py
+++ utils/common.py
@@ -8,19 +8,18 @@ import os
 
 
 def get_logger():
-    file = logging.FileHandler(
-        Path(__file__).parent.parent / "reports/logs/logfile.log"
-    )  # File for log
-    formatter = logging.Formatter(
-        "%(levelname)s :%(name)s :%(message)s :%(asctime)s",
-        datefmt="%Y-%m-%d %I:%M:%S %p",
-    )  # Format of log
-    file.setFormatter(formatter)  # set formatter into file
+    config_file_path = Path(__file__).parent.parent / 'config/logging.conf'
+    # Define a custom log filename
+    log_filename = "reports/logs/logfile.log"
+    # Check if the log directory exists; if not, create it
+    log_directory = os.path.dirname(log_filename)
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+        print(f"Log File Created in {log_directory}")
 
+    logging.config.fileConfig(config_file_path, defaults={'log_filename': log_filename})
     logger_name = inspect.stack()[1][3]
     logger = logging.getLogger(logger_name)
-    logger.addHandler(file)  # adding log into file
-    logger.setLevel(logging.INFO)
     return logger
 
 
@@ -42,3 +41,5 @@ def get_html_reports():
     except Exception as e:
         print("Report not ready, Error", e)
     return reports
+LOGGING_CONFIG = Path(__file__).parent.parent / 'config/logging.conf'
+print(LOGGING_CONFIG)
