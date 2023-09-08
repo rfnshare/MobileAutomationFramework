d89c1cff724e2dc381092ce8cc64a5cafa5b92e2
Abdullah Al Faroque <rfnshare@gmail.com>
2023-09-08 20:28:09 +0600

    added failed ss into html report, failed test cases network request, response(pening)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
 import pytest
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
-
+import datetime
 from utils.config import getConfig, setup_config
 
-appium_driver = None
+driver = None
 
 
 # Simulating a scenario where the Appium driver is not initialized
@@ -45,7 +47,7 @@ def set_and_get_config_data():
 
 @pytest.fixture(scope="function")
 def setup(request):
-    global appium_driver
+    global driver
     options = UiAutomator2Options()
     data = set_and_get_config_data()
     options.app = data["apkPath"]
@@ -59,3 +61,78 @@ def setup(request):
     request.cls.driver = driver
     yield
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+    Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+    :param item:
+    """
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    # Generate a timestamp in the format YYYY-MM-DD_HH-MM-SS
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%I-%M-%S-%p")
+
+    if report.when == "call" or report.when == "setup":
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # Start performance recording with the "Page.load" category
+
+            file_name = (report.nodeid.replace("::", "_")).replace("/", "_") + f"_{timestamp}.png"
+            SS_PATH = Path(__file__).parent.parent / "reports/screenshots/failed"
+            _capture_screenshot(SS_PATH / file_name)
+            if file_name:
+                image_path = Path(__file__).parent.parent / 'reports/screenshots/failed' / file_name
+                # Encode the path to HTML-safe format
+                encoded_path = image_path.as_uri()
+                html = (
+                    f'<div><img src="{encoded_path}" alt="screenshot" style="width:150px;height:300px;" '
+                    'onclick="window.open(this.src)" align="right"/></div>'
+                )
+                extra.append(pytest_html.extras.html(html))
+        report.extras = extra
+
+
+def _capture_screenshot(name):
+    try:
+        if driver is None:
+            raise AppiumDriverNotInitializedError
+        driver.get_screenshot_as_file(name)
+    except AppiumDriverNotInitializedError as e:
+        print(f"An error occurred: {e}")
+
+
+def pytest_exception_interact(node, call, report):
+    """
+        Pending Implementation: Setup API response (request & response) data in Log
+        """
+    if report.failed:
+        test_name = node.name  # Get the name of the test
+        test_file = node.parent.nodeid  # Get the test file path
+
+        exception_info = call.excinfo  # Get the ExceptionInfo instance
+        exception_type = exception_info.type
+        exception_value = exception_info.value
+
+        print(f"Test Name: {test_name}")
+        print(f"Test File: {test_file}")
+        print(f"Exception Type: {exception_type}")
+        print(f"Exception Value: {exception_value}")
+
+        # pending
+
+        # Retrieve network logs using the Appium driver
+        # network_logs = driver.get_log("performance")
+        # for entry in network_logs:
+        #     if "Network.requestWillBeSent" in entry["message"]["method"]:
+        #         request_data = entry["message"]["params"]["request"]
+        #         response_data = entry["message"]["params"]["response"]
+        #
+        #         # Process and log network request and response data
+        #         print("Request:")
+        #         print(request_data)
+        #         print("Response:")
+        #         print(response_data)
