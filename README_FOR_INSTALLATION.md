188e62f3ff5b24550848fb29f96c4d23d1e902ce
Abdullah Al Faroque <rfnshare@gmail.com>
2023-09-12 23:25:40 +0600

    added readme for setup appium, added curl support for auto install

---
+++ README_FOR_INSTALLATION.md
@@ -0,0 +1,8 @@
+Install Homebrew For Linux & macOS
+- /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+- (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/aalfaroque/.profile
+- eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+- sudo apt-get install build-essential
+
+For more information, see:
+https://docs.brew.sh/Homebrew-on-Linux
--- installation_script.py
+++ installation_script.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import subprocess
 import sys
 import getpass
@@ -11,6 +12,37 @@ def execute_sudo_command(command):
         return True
     else:
         return False
+
+
+def is_curl_installed():
+    try:
+        subprocess.run(["curl", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
+
+def install_curl():
+    system_platform = platform.system()
+
+    if system_platform == "Windows":
+        print("Curl installation on Windows is not supported via Python script.")
+        return
+    elif system_platform == "Darwin":  # macOS
+        try:
+            subprocess.run(["brew", "install", "curl"], check=True)
+            print("Curl installed successfully on macOS.")
+        except subprocess.CalledProcessError:
+            print("Failed to install curl on macOS.")
+    elif system_platform == "Linux":
+        try:
+            command = "apt install curl"
+            execute_sudo_command(command)
+            print("Curl installed successfully on Linux.")
+        except subprocess.CalledProcessError:
+            print("Failed to install curl on Linux.")
+    else:
+        print("Unsupported operating system.")
 
 
 def is_homebrew_installed():
@@ -37,14 +69,44 @@ def is_homebrew_installed():
         return False
 
 
+def install_homebrew():
+    pass  # will implement later, now install manually
+
+
 def check_and_install_dependency():
-    execute_sudo_command("apt update")
-    if is_homebrew_installed():
-        print("Homebrew is installed. You can continue with your Homebrew-related tasks.")
-        # Add your Homebrew-related tasks here
-    else:
-        print("Homebrew is not installed. Please install Homebrew and then continue.")
-        # Add instructions or code for installing Homebrew here
+    # execute_sudo_command("apt update")
+    # if is_homebrew_installed():
+    #     print("Homebrew is installed. You can continue with your Homebrew-related tasks.")
+    #     # Add your Homebrew-related tasks here
+    # else:
+    #     print("Homebrew is not installed. Please install Homebrew and then continue.")
+    #     # Add instructions or code for installing Homebrew here
+    try:
+        if sys.platform == "darwin" or sys.platform == "linux" or sys.platform == "linux2":  # macOS
+            if not is_curl_installed():
+                print("Curl are not installed. Installing curl...")
+                install_curl()
+
+                # Recheck if Node.js and npm are installed after the installation
+                if not is_curl_installed():
+                    print("Curl installation failed. Please install Curl manually.")
+                    return
+                else:
+                    print("Curl and npm installation successful.")
+            else:
+                print("Curl Found, Skipping Installation...")
+            return True
+        elif sys.platform == "win32":  # Windows
+            # Check if Homebrew is installed on Windows
+            # homebrew_install_path = r"C:\Program Files\Git\usr\bin\brew.exe"  # Update with the actual path
+            # subprocess.run([homebrew_install_path, "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            #                check=True)
+            print("Brew Check Skipping For Now For Windows")
+            return True
+        else:
+            return False  # Unsupported platform
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
 
 
 # Example usage:
